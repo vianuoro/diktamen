0890781cf00b2af2299759da9c91d7b5323111e9
vianuoro <35597425+vianuoro@users.noreply.github.com>
2025-03-23 23:18:21 +0100

    Update run.sh
    read sentence and then word by word

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-while IFS= read -r line; do for i in $line; do rm *.mp3; wget $(curl -s 'https://ttsmp3.com/makemp3_new.php' --data-raw "msg=$i&lang=Astrid&source=ttsmp3" | jq -r '.URL' | sed 's/\\//g'); ffplay -autoexit -t '231' *.mp3; done; done < lines.txt
+while IFS= read -r line; do 
+	wget $(curl -s 'https://ttsmp3.com/makemp3_new.php' --data-raw "msg=$line&lang=Astrid&source=ttsmp3" | jq -r '.URL' | sed 's/\\//g');
+	ffplay -autoexit -t '231' *.mp3;
+	rm *.mp3; 
+	for i in $line; do
+		rm *.mp3;
+		wget $(curl -s 'https://ttsmp3.com/makemp3_new.php' --data-raw "msg=$i&lang=Astrid&source=ttsmp3" | jq -r '.URL' | sed 's/\\//g');
+		ffplay -autoexit -t '231' -af "atempo=0.6" *.mp3; sleep 2;
+	done;
+done < lines.txt
