d276a85e8f38d258263bf2c0e00d7909185771c4
Your Name <you@example.com>
2025-03-24 14:12:00 +0100

    speak out punkt and komma

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # diktamen
 diktamen till Anna
+
+Packages: wget curl mpv sed jq
+Usage:
+- paste your text in file
+- create the readable file with command:
+  ./split_rows.sh text.txt > lines.txt
+- play the diktamen with ./run.sh
--- split_rows.sh
+++ split_rows.sh
@@ -11,6 +11,6 @@ processed_text=$(sed 's/,/ komma /g' "$INPUT_FILE")  # Replace commas with "komm
 processed_text=$(echo "$processed_text" | sed ':a;N;$!ba;s/\n/ nyrad /g')  # Replace newlines with "ny rad"
 # Step 2: Split sentences that end with a period
 # Replace period followed by a space or end of the string with a new line
-processed_text=$(echo "$processed_text" | sed 's/\.\s*/. nyrad /g')
+processed_text=$(echo "$processed_text" | sed 's/\.\s*/ punkt nyrad /g')
 # Step 3: Output each sentence as a standalone line
 echo "$processed_text" | sed 's/ nyrad /\'$'\n/g'
