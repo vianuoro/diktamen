1ae85258647966967c524eab678381d52b7c5c02
Your Name <you@example.com>
2025-03-24 13:44:52 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,14 @@ rm *.mp3
 while IFS= read -r line; do 
         wget $(curl -s 'https://ttsmp3.com/makemp3_new.php' --data-raw "msg=$line&lang=Astrid&source=ttsmp3" | jq -r '.URL' | sed 's/\\//g');
         mpv *.mp3;
-        rm *.mp3; 
+        rm --speed=0.9 --ao=pulse *.mp3; 
         for i in $line; do
             rm *.mp3;
             wget $(curl -s 'https://ttsmp3.com/makemp3_new.php' --data-raw "msg=$i&lang=Astrid&source=ttsmp3" | jq -r '.URL' | sed 's/\\//g');
-            mpv *.mp3; sleep 2;
+            mpv --speed=0.7 --ao=pulse *.mp3; sleep 1;
 	done;
+	rm *.mp3;
+	wget $(curl -s 'https://ttsmp3.com/makemp3_new.php' --data-raw "msg=$line&lang=Astrid&source=ttsmp3" | jq -r '.URL' | sed 's/\\//g');
+	mpv --speed=0.7 --ao=pulse *.mp3;
+	rm *.mp3;
 done < lines.txt
