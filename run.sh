0e6422cb614ae69dfce91ca73e02aef4cb83e2c3
Your Name <you@example.com>
2025-03-29 14:48:41 +0100

    improve slow motion sound

--- run.sh
+++ run.sh
@@ -1,15 +1,19 @@
 rm *.mp3
 while IFS= read -r line; do 
         wget $(curl -s 'https://ttsmp3.com/makemp3_new.php' --data-raw "msg=$line&lang=Astrid&source=ttsmp3" | jq -r '.URL' | sed 's/\\//g');
-        mpv *.mp3;
-        rm --speed=0.9 --ao=pulse *.mp3; 
+        ffmpeg -i *.mp3 -filter:a "atempo=0.8" file_0.8.mp3
+	mpv file_0.8.mp3;
+        rm *.mp3;
         for i in $line; do
             rm *.mp3;
             wget $(curl -s 'https://ttsmp3.com/makemp3_new.php' --data-raw "msg=$i&lang=Astrid&source=ttsmp3" | jq -r '.URL' | sed 's/\\//g');
-            mpv --speed=0.7 --ao=pulse *.mp3; sleep 1;
+            ffmpeg -i *.mp3 -filter:a "atempo=0.8" file_0.8.mp3
+            mpv file_0.8.mp3;
+	    sleep 2;
 	done;
 	rm *.mp3;
-	wget $(curl -s 'https://ttsmp3.com/makemp3_new.php' --data-raw "msg=$line&lang=Astrid&source=ttsmp3" | jq -r '.URL' | sed 's/\\//g');
-	mpv --speed=0.7 --ao=pulse *.mp3;
-	rm *.mp3;
+        wget $(curl -s 'https://ttsmp3.com/makemp3_new.php' --data-raw "msg=$line&lang=Astrid&source=ttsmp3" | jq -r '.URL' | sed 's/\\//g');
+        ffmpeg -i *.mp3 -filter:a "atempo=0.5" file_0.5.mp3
+        mpv file_0.5.mp3;
+        rm *.mp3;
 done < lines.txt
